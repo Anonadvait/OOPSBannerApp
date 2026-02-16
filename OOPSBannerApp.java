91afe7f563d4945b81a05e7e5cb0513ceac93bb5
Advait Awasthi <awasthiadvait27@gmail.com>
2026-02-16 21:16:13 +0530

    UC2: Print OOPS in banner format

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,16 +1,17 @@
-/**
- * OOPSBannerApp
- *
- * This application prints the literal text "OOPS" to the console.
- *
- * @author Erza
- * @version 1.0
- */
+
 
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println(" ********   ********   ********   ******** ");
+        System.out.println("*        * *        * *        * *        ");
+        System.out.println("*        * *        * *        * *        ");
+        System.out.println("*        * *        * ********   ******** ");
+        System.out.println("*        * *        * *                  ");
+        System.out.println("*        * *        * *                  ");
+        System.out.println(" ********   ********   *         ******** ");
+
     }
 
 }
