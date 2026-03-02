f3041644c582ca7d17c3df3e7ef6a78caf8dae2f
Advait Awasthi <awasthiadvait27@gmail.com>
2026-03-03 05:22:11 +0530

    UC6: Modularize banner using static helper methods

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,18 +2,48 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] banner = {
-                String.join("   ", " ******** ", " ******** ", " ******** ", " ******** "),
-                String.join("   ", "*        *", "*        *", "*        *", "*        "),
-                String.join("   ", "*        *", "*        *", "*        *", "*        "),
-                String.join("   ", "*        *", "*        *", "********  ", " ******** "),
-                String.join("   ", "*        *", "*        *", "*         ", "         *"),
-                String.join("   ", "*        *", "*        *", "*         ", "         *"),
-                String.join("   ", " ******** ", " ******** ", "*         ", " ******** ")
-        };
+        String[] O = getO();
+        String[] P = getP();
+        String[] S = getS();
 
-        for (String line : banner) {
-            System.out.println(line);
+        for (int i = 0; i < O.length; i++) {
+            System.out.println(O[i] + "   " + O[i] + "   " + P[i] + "   " + S[i]);
         }
+    }
+
+    public static String[] getO() {
+        return new String[] {
+                " ******** ",
+                "*        *",
+                "*        *",
+                "*        *",
+                "*        *",
+                "*        *",
+                " ******** "
+        };
+    }
+
+    public static String[] getP() {
+        return new String[] {
+                " ******** ",
+                "*        *",
+                "*        *",
+                " ******** ",
+                "*         ",
+                "*         ",
+                "*         "
+        };
+    }
+
+    public static String[] getS() {
+        return new String[] {
+                " ******** ",
+                "*         ",
+                "*         ",
+                " ******** ",
+                "         *",
+                "         *",
+                " ******** "
+        };
     }
 }
