f13e900b2e27e087bc52cd539c9c98ccc1fd3d58
Advait Awasthi <awasthiadvait27@gmail.com>
2026-03-03 08:18:11 +0530

    UC7:Encapsulate banner paterns in CharecterPattern class

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,18 +2,68 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] banner = {
-                String.join("   ", " ******** ", " ******** ", " ******** ", " ******** "),
-                String.join("   ", "*        *", "*        *", "*        *", "*        "),
-                String.join("   ", "*        *", "*        *", "*        *", "*        "),
-                String.join("   ", "*        *", "*        *", "********  ", " ******** "),
-                String.join("   ", "*        *", "*        *", "*         ", "         *"),
-                String.join("   ", "*        *", "*        *", "*         ", "         *"),
-                String.join("   ", " ******** ", " ******** ", "*         ", " ******** ")
-        };
+        String word = "OOPS";
 
-        for (String line : banner) {
+        CharacterPattern O = new CharacterPattern('O', new String[] {
+                " ******** ",
+                "*        *",
+                "*        *",
+                "*        *",
+                "*        *",
+                "*        *",
+                " ******** "
+        });
+
+        CharacterPattern P = new CharacterPattern('P', new String[] {
+                " ******** ",
+                "*        *",
+                "*        *",
+                " ******** ",
+                "*         ",
+                "*         ",
+                "*         "
+        });
+
+        CharacterPattern S = new CharacterPattern('S', new String[] {
+                " ******** ",
+                "*         ",
+                "*         ",
+                " ******** ",
+                "         *",
+                "         *",
+                " ******** "
+        });
+
+        CharacterPattern[] patterns = { O, O, P, S };
+
+        for (int i = 0; i < 7; i++) {
+            StringBuilder line = new StringBuilder();
+
+            for (CharacterPattern cp : patterns) {
+                line.append(cp.getPattern()[i]).append("   ");
+            }
+
             System.out.println(line);
+        }
+    }
+
+    // Static Inner Class
+    static class CharacterPattern {
+
+        private char character;
+        private String[] pattern;
+
+        public CharacterPattern(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        public char getCharacter() {
+            return character;
+        }
+
+        public String[] getPattern() {
+            return pattern;
         }
     }
 }
